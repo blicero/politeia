8a509f79a54727f8ad29925d2144ddc243620a37
Benjamin Walkenhorst <krylon@posteo.de>
2025-09-18 16:38:22 +0200

    Add module model

---
+++ model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-09-18 16:38:19 krylon>
+#
+# /data/code/python/politeia/model.py
+# created on 18. 09. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+politeia.model
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+
+from dataclasses import dataclass
+
+
+@dataclass(slots=True, kw_only=True)
+class Polis:
+    """The central element in our game, a city state set in a world modeled on ancient Greece/Ionia."""
+
+    name: str
+    population: int
+
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
