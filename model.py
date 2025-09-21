9b7b0528b3987b60853e09136c676a901ff0c18a
Benjamin Walkenhorst <krylon@posteo.de>
2025-09-21 19:06:31 +0200

    model - extend Polis

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-09-18 22:13:03 krylon>
+# Time-stamp: <2025-09-20 18:43:57 krylon>
 #
 # /data/code/python/politeia/model.py
 # created on 18. 09. 2025
@@ -19,6 +19,7 @@ politeia.model
 
 from dataclasses import dataclass, field
 from enum import Enum, auto
+from typing import Final
 
 
 class Work(Enum):
@@ -36,9 +37,28 @@ class Polis:
     """The central element in our game, a city state set in a world modeled on ancient Greece/Ionia."""
 
     name: str
-    population: int
-    food: int
+    population: int = 0
+    food: int = 0
     work: map[Work, int] = field(default_factory=dict)
+
+    @property
+    def free(self) -> int:
+        """Return the number of unused population."""
+        return self.population - sum(self.work.values())
+
+    def assign(self, task: Work, cnt: int) -> None:
+        """Assign the given number of workers to the given task."""
+        free: Final[int] = self.free
+        if free < cnt:
+            raise ValueError(f"Invalid count {cnt} ({free} available workers)")
+        elif cnt <= 0:
+            raise ValueError(f"Invalid count {cnt} (must be > 0)")
+
+        if task not in self.work:
+            self.work[task] = 0
+
+        self.work[task] += cnt
+
 
 # Local Variables: #
 # python-indent: 4 #
